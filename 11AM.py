afaaf3c48ecec5e14a5879d58bec121e7c45a4db
notmahsa-personal <m_naseri76@yahoo.com>
2018-01-27 12:55:03 -0500

    Pep8

--- 11AM.py
+++ 11AM.py
@@ -1,21 +1,18 @@
-import numpy
+import numpy as np
+import os
 import csv
+import matplotlib.pyplot as plt
+
+
 def data_read(file_name):
     datafile = open(file_name, 'r')
     datareader = csv.reader(datafile, delimiter=';')
     data = []
     for row in datareader:
         data.append(row)
-    return data
-print (data_read('hackathon_dataset_2009.csv')[0])
+    print file_name, data[0]
+    return np.array(data[1:])
 
-import numpy as np
-import os
-import csv
-
-
-def data_read(filename):
-    pass
 
 def hypothesis(w, phi):
     h = np.matmul(w.transpose(), phi.transpose())
@@ -51,12 +48,9 @@ def learn(m):
 
 
 if __name__ == "__main__":
-
-    '''
-    import matplotlib.pyplot as plt
-    plt.plot([1,2,3,4])
-    plt.ylabel('some numbers')
-    plt.show()
-    '''
-    import matplotlib.pyplot as plt
     pass
+
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
+print (data_read('hackathon_dataset_2009.csv')[0])
