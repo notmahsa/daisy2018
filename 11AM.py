4e1e1c0bdcde364469f947af44f0349fc55116f5
notmahsa-personal <m_naseri76@yahoo.com>
2018-01-27 12:15:50 -0500

    12 seconds

--- 11AM.py
+++ 11AM.py
@@ -1,8 +1,17 @@
 import numpy
 import csv
-datafile = open('hackathon_dataset_2009.csv', 'r')
-datareader = csv.reader(datafile, delimiter=';')
-data = []
-for row in datareader:
-    data.append(row)
-print data[0]
+def data_read(file_name):
+    datafile = open(file_name, 'r')
+    datareader = csv.reader(datafile, delimiter=';')
+    data = []
+    for row in datareader:
+        data.append(row)
+    return data
+print data_read('hackathon_dataset_2009.csv')[0]
+'''
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
+'''
+import matplotlib.pyplot as plt
